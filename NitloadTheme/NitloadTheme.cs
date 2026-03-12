74475f6f1687fe9464b73465f5f43ea796ee6c4d
Nitload <105042599+Nitload-NSI@users.noreply.github.com>
2026-03-13 00:49:03 +0800

    Rebuild theme: full NordFluent base (91 categories) + Tundora Shell overlay

--- NitloadTheme/NitloadTheme.cs
+++
@@ -1,8 +0,0 @@
-namespace NitloadTheme
-{
-    // This file is a placeholder required by the VSIX project type.
-    // All theme colors are defined in NitloadTheme.vstheme.
-    internal class NitloadTheme
-    {
-    }
-}
