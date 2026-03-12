a45be910504fe6f53af599edc1222b129ce3d6a9
Nitload <105042599+Nitload-NSI@users.noreply.github.com>
2026-03-13 00:20:51 +0800

    Initial commit: NitloadTheme VS2026 hybrid theme

---
+++ NitloadTheme/NitloadTheme.cs
@@ -0,0 +1,8 @@
+namespace NitloadTheme
+{
+    // This file is a placeholder required by the VSIX project type.
+    // All theme colors are defined in NitloadTheme.vstheme.
+    internal class NitloadTheme
+    {
+    }
+}
